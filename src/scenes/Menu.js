45bd79fd50ccaa0fcc3cddd1d1361cf6ac146a04
Huy Nguyen <mitanguy@ucsc.edu>
2022-04-18 12:41:26 -0700

    update

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -15,6 +15,30 @@ class Menu extends Phaser.Scene {
 
         let menuConfig = {
             fontFamily: 'Lucida Console',
+            fontSize: '20px',
+            color: '#ffffff',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        
+        let controlsConfig = {
+            fontFamily: 'Lucida Console',
+            fontSize: '15px',
+            color: '#ffffff',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        let titleConfig = {
+            fontFamily: 'Lucida Console',
             fontSize: '28px',
             color: '#ffffff',
             align: 'right',
@@ -24,8 +48,10 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
-        this.add.text(game.config.width/2, game.config.height/2, 'Use the mouse to move & (F) or (LeftMouseClick) to fire', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET 🚀 PATROL', menuConfig).setOrigin(0.5);
+
+
+        this.add.text(game.config.width/2, game.config.height/2, 'Use the mouse to move & (F) or (LeftMouseClick) to fire', controlsConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET 🚀 PATROL', titleConfig).setOrigin(0.5);
         menuConfig.color = '#FFFFFFFF';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice Difficulty', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize * 2.7, 'Press → for Expert Difficulty', menuConfig).setOrigin(0.5);
